ba0b13d5c848b824bfd522af019399e9643ccb4d
Abhijeet Gawde <Abhijeet.gawde@capgemini.com>
2026-08-21 13:30:26 +0530

    changes: enhancement to randomize correct option order

--- src/services/questionnaireService.js
+++ src/services/questionnaireService.js
@@ -3,10 +3,25 @@ const questionnaires = import.meta.glob('../data/questionnaires/*.json', {
   import: 'default'
 });
 
+function shuffleOptions(options) {
+  const shuffled = [...options];
+  for (let index = shuffled.length - 1; index > 0; index -= 1) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [shuffled[index], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[index]];
+  }
+  return shuffled;
+}
+
 export async function getQuestionnaire(topicId) {
   const questionnaire = questionnaires[`../data/questionnaires/${topicId}.json`];
   if (!questionnaire) {
     throw new Error('This topic is not available.');
   }
-  return questionnaire;
+  return {
+    ...questionnaire,
+    questions: questionnaire.questions.map((question) => ({
+      ...question,
+      options: shuffleOptions(question.options)
+    }))
+  };
 }
